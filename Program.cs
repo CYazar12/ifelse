c9a0eac7cc68d62dd4c92c710823b463ff3b0189
Celale Yildiz Yazar <celale@hotmail.de>
2022-07-28 00:19:38 +0200

    IF ELSE

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+namespace ifelse
+{
+    class Program
+    {
+        public static void Main( string[] args)
+        {
+int time = DateTime.Now.Hour;
+        
+
+if(time>=6 && time <= 12)
+{
+    Console.WriteLine("iyi günler!");
+}
+else if (time>12 && time<=18)
+{
+    Console.WriteLine("iyi günler!");
+}
+else
+{
+    Console.WriteLine("iyi geceler!");
+}
+Console.WriteLine(time >= 6 && time <= 12 ? "iyi günler" : time > 12 && time <= 18 ? "iyi günler" : "iyi geceler");
+        }
+    }
+ }
